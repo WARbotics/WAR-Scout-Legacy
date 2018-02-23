e6d11acf3a7f7f4ec66c74fa4d67631973861c50
vhenrixon <vhenrixon@gmail.com>
2018-02-23 15:17:55 -0500

    fix

--- MyApp.py
+++ MyApp.py
@@ -65,25 +65,6 @@ class WARscout:
                 for row in values:
                     list.append(int(row[0]))
                     list.append(int(row[1]))
-<<<<<<< HEAD
-            global endh
-            global endm
-            getEndh = list.index(99) - 2
-            getEndm = list.index(99) - 1
-            endh = list[getEndh]
-            endm = list[getEndm]
-        except:
-            if sheet == 'Current':
-                list = [8,0,8,30,8,35,9,16,9,21,10,2,10,7,10,48,10,53,
-                        11,34,11,39,12,4,12,9,12,50,12,55,13,36,13,41,
-                        14,22,14,27,14,57,15,35,99,99]
-            if sheet == 'CurrentF':
-                list = [8,0,8,30,8,35,9,19,9,24,10,8,10,16,11,0,
-                        11,5,11,49,11,54,12,19,12,24,13,8,13,16,14,0,
-                        14,5,14,49,15,25,99,99]
-            print('Error')
-=======
->>>>>>> bba062b4db78b7ddef244a78e91ee5bb3f736dd9
 
 class Data:
     key = ""
@@ -94,16 +75,9 @@ class Data:
     def getStatus():
         return Status()
     def __init__():
-<<<<<<< HEAD
         tba.event_rankings(event)
         print "WAR botics FRC scouting script"
     def getTeamData(teamNumber):
         # get the team number from google spread sheet
         yearTeam = tba.team_years(teamNumber)
         robotYear = tba.team_robots(teamNumber)
-=======
-        print "WAR botics FRC scouting script"
-    def getTeamData(teamNumber):
-        team = # get the team number from google spread sheet
-
->>>>>>> bba062b4db78b7ddef244a78e91ee5bb3f736dd9
