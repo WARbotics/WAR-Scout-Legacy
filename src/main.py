f96378d2c2fecd64c18c3e26110dfbe66f6aa207
Trey <techking802@gmail.com>
2019-03-12 23:14:00 -0400

    Needs Polish

--- src/main.py
+++ src/main.py
@@ -31,17 +31,25 @@ df = pd.DataFrame(team_list, index=team_list, columns=['ID'])
 #print('DONE')
 
 class Team:
+    def checkNull(self, x):
+        if x == '':
+            return(0)
+        else:
+            return(x)
+
     def average_list(self, input):
         holder = 0
         for num in input:
-            holder += float(num)
+            holder += float(self.checkNull(num))
         return(round(holder / len(list), 1))
 
+    def percent(self, input):
+        return(str(round(input*100, 1))+'%')
     def find_data(self):
         print("Finding Types... ", end="\r", flush=True)
         df['Type'], df['Ball Avg'], df['Hatch Avg'] = None, None, None
         for team_mod in team_list:
-            output, actionDict = {'type': 'Uknown', 'start_type': 'Uknown'}, {}
+            output, actionDict = {'type': 'Uknown', 'start_type': 'Uknown', 'Line Cross': ''}, {}
             for n in raw_team_data:
                 if n[0] == team_mod:
                     for key, val in QT:
@@ -51,6 +59,8 @@ class Team:
             ball_avg = self.average_list(actionDict['TCargo'])
             sHatch_avg = self.average_list(actionDict['SHatch'])
             sBall_avg = self.average_list(actionDict['SCargo'])
+            output['Driving'] = self.average_list(actionDict['driving'])
+            output['Contribute'] = self.average_list(actionDict['help'])
             #----Teleop Phase
             if hatch_avg == ball_avg:
                 output['type'] = 'Mixed'
@@ -65,6 +75,8 @@ class Team:
             elif ball_avg < 1:
                 if hatch_avg < 1:
                     output['type'] = 'Vegetable'
+            else:
+                output['type'] = 'No Plays'
             #---Sandstorm Phase
             if sHatch_avg == sBall_avg:
                 output['start_type'] = 'Mixed'
@@ -75,6 +87,8 @@ class Team:
             elif sBall_avg < 1:
                 if sHatch_avg < 1:
                     output['start_type'] = 'No Plays'
+            else:
+                output['start_type'] = 'No Plays'
             #---Start Position
             if actionDict['startPos'].count('L') > actionDict['startPos'].count('R'):
                 if actionDict['startPos'].count('L') > actionDict['startPos'].count('M'):
@@ -87,8 +101,28 @@ class Team:
                     output['startPos'] = 'Middle'
             else:
                 output['startPos'] = 'Mixed'
-            print('Finding Types... '+str(team_list.index(team_mod)/len(team_list)), end="%\r")
-            df.loc[team_mod]['Type'], df.loc[team_mod]['Ball Avg'], df.loc[team_mod]['Hatch Avg'] = type, ball_avg, hatch_avg
+            #---Line Cross
+            output['Line Cross'] = ('Overall: '+(self.percent((actionDict['lineCross'].count('Level 1') + actionDict['lineCross'].count('Level 2'))/len(actionDict['lineCross'])))+' | Level 2: '+self.percent(actionDict['lineCross'].count('Level 2')/len(actionDict['lineCross']))+' | Level 1: '+self.percent(actionDict['lineCross'].count('Level 1')/len(actionDict['lineCross'])))
+            #---Climb 2+
+            output['Climb 2+'] = self.percent((actionDict['climb'].count('Level 2') + actionDict['climb'].count('Level 3'))/ len(actionDict['climb']))
+            #---Playstyle
+            if actionDict['playStyle'].count('Defensive') > actionDict['playStyle'].count('Aggressive'):
+                if actionDict['playStyle'].count('Defensive') > actionDict['playStyle'].count('MBoth'):
+                    output['startPos'] = 'Defensive'
+            elif actionDict['playStyle'].count('Aggressive') > actionDict['playStyle'].count('Defensive'):
+                if actionDict['playStyle'].count('Aggressive') > actionDict['playStyle'].count('Both'):
+                    output['startPos'] = 'Agressive'
+            elif actionDict['playStyle'].count('Both') > actionDict['playStyle'].count('Defensive'):
+                if actionDict['playStyle'].count('Both') > actionDict['playStyle'].count('Aggressive'):
+                    output['startPos'] = 'Both'
+            else:
+                output['startPos'] = 'Mixed'
+            #--Final
+            for key, val in output:
+                df[key] = None
+                df.loc[team_mod][key] = val
+
+            print('Finding Types... '+self.percent(team_list.index(team_mod)/len(team_list)), end="%\r")
         print('Finding Types... DONE!')
 
     
