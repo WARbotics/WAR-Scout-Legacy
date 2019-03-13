b5112a0b30a6846dc3269132bda732d9696f6cde
Trey <techking802@gmail.com>
2019-03-13 13:20:19 -0400

    Stable Build

--- src/main.py
+++ src/main.py
@@ -1,31 +1,10 @@
 import dataCollection as data
 import pandas as pd
+import export
 import tbapy
 import operator
 
-dependent_vars, raw_team_data = data.getSheet('B1:O1')[0], data.getSheet('B2:O')
-dlen, tlen = len(dependent_vars), len(raw_team_data)
-QT = {
-    'ID': dependent_vars.index('ID of the Team'),
-    'THatch': dependent_vars.index('How many hatches did they mount?'), 
-    'TCargo': dependent_vars.index('How much cargo did they secure?'), 
-    'SHatch': dependent_vars.index('How many S:Hatches?'), 
-    'SCargo': dependent_vars.index('How many S:Cargo?'),
-    'lineCross': dependent_vars.index('Crossed HAB Line?'), 
-    'startPos': dependent_vars.index('Starting Position'),
-    'climb': dependent_vars.index('HAB Climbing'),
-    'playStyle': dependent_vars.index('What was the robot\'s play style?'),
-    'help': dependent_vars.index('How much did the robot contribute to the team?'),
-    'driving': dependent_vars.index('How would you rate the team\'s driving?'),
-    'free': dependent_vars.index('Free Response (No more than a sentence or two)')
-    }
 
-team_list = []
-for team in raw_team_data:
-    if team[0] not in team_list:
-        team_list.append(str(team[0]))
-
-df = pd.DataFrame(team_list, index=team_list, columns=['ID'])
 #print("Creating Dataframe... ", end="", flush=True)
 #df.set_index(['ID of the Team'])
 #print('DONE')
@@ -41,24 +20,57 @@ class Team:
         holder = 0
         for num in input:
             holder += float(self.checkNull(num))
-        return(round(holder / len(list), 1))
+        return(round(holder / len(input), 1))
 
     def percent(self, input):
-        return(str(round(input*100, 1))+'%')
+        x = round(input*100, 1)
+        try:
+            return(str(int(x))+'%')
+        except:
+            return(str(x)+'%')
     def find_data(self):
+        dependent_vars, raw_team_data = data.getSheet('B1:O1')[0], data.getSheet('B2:O')
+        dlen, tlen = len(dependent_vars), len(raw_team_data)
+        QT = {
+            'ID': dependent_vars.index('ID of the Team'),
+            'THatch': dependent_vars.index('How many hatches did they mount?'), 
+            'TCargo': dependent_vars.index('How much cargo did they secure?'), 
+            'SHatch': dependent_vars.index('How many S:Hatches?'), 
+            'SCargo': dependent_vars.index('How many S:Cargo?'),
+            'lineCross': dependent_vars.index('Crossed HAB Line?'), 
+            'startPos': dependent_vars.index('Starting Position'),
+            'climb': dependent_vars.index('HAB Climbing'),
+            'playStyle': dependent_vars.index('What was the robot\'s play style?'),
+            'help': dependent_vars.index('How much did the robot contribute to the team?'),
+            'driving': dependent_vars.index('How would you rate the team\'s driving?'),
+            'free': dependent_vars.index('Free Response (No more than a sentence or two)')
+        }
+
+        team_list = []
+        for team in raw_team_data:
+            if team[0] not in team_list:
+                team_list.append(str(team[0]))
+
+        df = pd.DataFrame(index=team_list)
         print("Finding Types... ", end="\r", flush=True)
-        df['Type'], df['Ball Avg'], df['Hatch Avg'] = None, None, None
         for team_mod in team_list:
-            output, actionDict = {'type': 'Uknown', 'start_type': 'Uknown', 'Line Cross': ''}, {}
+            output, actionDict = {'Line Cross': ''}, {}
             for n in raw_team_data:
                 if n[0] == team_mod:
-                    for key, val in QT:
-                        actionDict[key] = n[val]
+                    for key, val in QT.items():
+                        try:
+                            actionDict[key] += [n[val]]
+                        except KeyError:
+                            actionDict[key] = [n[val]]
             #-----Averages
             hatch_avg = self.average_list(actionDict['THatch'])
             ball_avg = self.average_list(actionDict['TCargo'])
             sHatch_avg = self.average_list(actionDict['SHatch'])
             sBall_avg = self.average_list(actionDict['SCargo'])
+            output['Tele Hatch'] = hatch_avg
+            output['Tele Cargo'] = ball_avg
+            output['Sand Hatch'] = sHatch_avg
+            output['Sand Cargo'] = sBall_avg
             output['Driving'] = self.average_list(actionDict['driving'])
             output['Contribute'] = self.average_list(actionDict['help'])
             #----Teleop Phase
@@ -118,19 +130,17 @@ class Team:
             else:
                 output['startPos'] = 'Mixed'
             #--Final
-            for key, val in output:
+            for key, val in output.items():
                 df[key] = None
-                df.loc[team_mod][key] = val
+                df.loc[team_mod][key] = str(val)
 
             print('Finding Types... '+self.percent(team_list.index(team_mod)/len(team_list)), end="%\r")
         print('Finding Types... DONE!')
-
+        df.to_excel("output.xlsx")
     
+        return(df)
 
-        
-        
+  
 
 if __name__ == '__main__':
-    Team().find_data()
-    #df.loc['6925'].append('Hello')
-    #print(df)
+    print(Team().find_data())
